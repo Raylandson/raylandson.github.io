3561d61a22bbdba9e1e437ec882d25dfea911eb0
Raylandson <www.raylandsoncs@gmail.com>
2023-04-28 00:05:20 -0300

    initial project

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+let myImage = document.querySelector('img');
+let myButton = document.querySelector('button'); 
+
+myImage.onclick = () => {
+	const mySrc = myImage.getAttribute("src");
+	if (mySrc === "images/mai-dragon-in-love.gif") {
+	    myImage.setAttribute("src", "images/flustered-anime.gif");
+	} else {
+	    myImage.setAttribute("src", "images/mai-dragon-in-love.gif");
+	}
+};
+
+
+function setLoveName() {
+	const name = prompt("Você me ama?");
+	localStorage.setItem("name", name);
+
+	if (['nao', 'não', 'no', 'not', 'n'].includes(name.toLowerCase())) {
+		alert('Poxa ;( tudo acabado');
+	} else if (['sim', 'si', 'yes', 's', 'y', 'yep'].includes(name.toLowerCase())) {
+		alert('Eu te amo também <3 S2 ❤💕😍😘');
+	} else {
+		alert('Resposta recusada, digitar novamente')
+		setLoveName()
+	};
+};
+
+if (!localStorage.getItem("name")) {
+	setLoveName();
+} else {
+	const storedName = localStorage.getItem("name");
+};
+
+myButton.onclick = () => {
+	setLoveName();
+};
